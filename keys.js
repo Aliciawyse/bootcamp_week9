065498258763db7d3043e262330d63bd7b2b3d35
Barrett <Aliciawyse@users.noreply.github.com>
2017-10-19 08:16:30 -0400

    added info for twitter command to work

---
+++ keys.js
@@ -0,0 +1,11 @@
+//Why does this show up in the command line.
+//console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: 'bEdzkhZM2cawYWlXz0pQWOwxD',
+    consumer_secret: 'MQdupi1VUL74z3ev9YEVA2PkvvYcRnrRmu5XyZitAbsO42Cobl',
+    access_token_key: '917108980878594049-iDXlMvEaCw1rJJojHNZTMgfifEuxjLy',
+    access_token_secret: 'pdRoXdAuESvMeZNrwuzLCd8yXTNR5eYK76tA0N9ox3Bp5'
+};
+
+module.exports = twitterKeys;
